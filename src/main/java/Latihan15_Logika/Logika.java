4c1ce5039d4ccbb8ab831f0663694a8784f0a651
afifarn <afifr0837@gmail.com>
2019-09-25 15:53:55 +0700

    first commit

---
+++ src/main/java/Latihan15_Logika/Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan15_Logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Afif Raditya Nugraha
+ * KELAS    : PBO_ULANG/IF4
+ * NIM      : 10116138
+ * LOG      : Program ini berisi program untuk menampilkan logika true or false
+ */
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) =" + !(a && b));
+    }
+    
+}
